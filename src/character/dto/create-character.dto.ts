15061159765d7fbc9351b2d148e8332f449fa11f
Jssoff <js.garay@uniandes.com>
2025-11-27 07:55:57 -0500

    dto

--- src/character/dto/create-character.dto.ts
+++ src/character/dto/create-character.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCharacterDto {}
+/* eslint-disable prettier/prettier */
+
+import { IsString, IsNumber, IsBoolean} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateCharacterDto {
+  @IsString()
+  name: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  salary: number;
+
+  @Type(() => Boolean)
+  @IsBoolean()
+  employee: boolean;
+}
--- src/location/dto/create-location.dto.ts
+++ src/location/dto/create-location.dto.ts
@@ -1,1 +1,19 @@
-export class CreateLocationDto {}
+/* eslint-disable prettier/prettier */
+import { IsString, IsNumber, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateLocationDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  type: string;
+
+  @Type(() => Number)
+  @IsNumber()
+  cost: number;
+
+  @Type(() => Number)
+  @IsInt()
+  ownerId: number;
+}
