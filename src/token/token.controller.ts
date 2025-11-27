e7fd681818614964e365a480369c42dbe2618ac8
Jssoff <js.garay@uniandes.com>
2025-11-27 10:46:48 -0500

    Fi modulo

--- src/token/token.controller.ts
+++ src/token/token.controller.ts
@@ -1,26 +1,25 @@
 /* eslint-disable prettier/prettier */
 
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TokenService } from './token.service';
+import { CreateTokenDto } from './dto/create-token.dto';
 
 @Controller('token')
 export class TokenController {
   constructor(private readonly tokenService: TokenService) {}
 
   @Post()
-  create(@Body('reqLeft') reqLeft: number) {
-    const initial = reqLeft ?? 10;
-    return this.tokenService.create(initial);
+  create(@Body() createTokenDto: CreateTokenDto) {
+    return this.tokenService.create(createTokenDto);
   }
 
-  @Get(':id')
-  get(@Param('id') id: string) {
-    const token = this.tokenService.find(id);
-    return { usable: token.active && token.reqLeft > 0, token };
+  @Get()
+  findOne(@Query('token') token: string) {
+    return this.tokenService.find(token);
   }
 
-  @Patch('reduce/:id')
-  reduce(@Param('id') id: string) {
+  @Patch(':id/reduceReqLeft')
+  reduceReqLeft(@Param('id') id: string) {
     return this.tokenService.reduce(id);
   }
 
--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -3,8 +3,11 @@
 import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
 import { TokenController } from './token.controller';
+import { TokenEntity } from './entities/token.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TokenEntity])],
   controllers: [TokenController],
   providers: [TokenService],
   exports: [TokenService],
